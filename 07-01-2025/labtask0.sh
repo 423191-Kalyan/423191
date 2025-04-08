67204466b193fbae32563930ebf3a8955f514425
423191-Kalyan <chaitanya2006varma@gmail.com>
2025-04-08 13:00:43 +0530

    Create labtask0.sh

---
+++ 07-01-2025/labtask0.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Q1 - use mkdir to create a directory
+mkdir 423191
+ls
+
+#Q2 - use cd and mkdir commands 
+cd 423191
+mkdir Kalyan
+ls
+
+#Q3 - use cd command to change the directory
+cd 423191
+cd Kalyan
+
+#Q4 - use cal command to get calendar of current month
+cal
+
+#Q5 - go to the parent directory and list the files in it 
+cd ../
+ls
