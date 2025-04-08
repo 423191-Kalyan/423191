9e99e76632b59cec48d38a857108da192dd54981
423191-Kalyan <chaitanya2006varma@gmail.com>
2025-04-08 13:10:49 +0530

    Create labtask1.sh

---
+++ 21-01-2025/labtask1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+#Q1 - To print the name of OS, login name, host name.
+
+uname
+whoami
+hostname
+
+#Q2 - To get calendar of the respective month
+cal 01 2024
+cal 02 1995
+cal 09 7
+cal
+date +"%a %b %Y"
+
+#Q3 - To get date
+date +"%T"
+date +"%r"
+
+#Q4 - To get the date
+date 
+
+#Q5 - To display in enlarged manner.
+banner good morning
